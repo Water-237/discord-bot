16038dd4a0e619cbfc9662bc8e03b52ec673eecf
Water-237 <107559349+Water-237@users.noreply.github.com>
2024-07-09 01:37:01 +0900

    メッセージボット 作成

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, SnowflakeUtil } = require('discord.js');
 
 const client = new Client(
     { intents: [
@@ -6,11 +6,23 @@ const client = new Client(
       GatewayIntentBits.GuildMessages,
       GatewayIntentBits.MessageContent
       ]
-    }
+
+  }
   );
 
   client.on('ready', () => {
     console.log('起動成功');
   });
-  client.login("--BOT TOKEN--");
 
+
+  client.on('messageCreate', message => {
+    if (message.content === 'ボーダー') {
+      try {
+        message.channel.send('テスト送信');
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  });
+
+client.login('TOKEN');
