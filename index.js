13008f7f384713b8a5b830973404eba1ecd0de62
Water-237 <107559349+Water-237@users.noreply.github.com>
2024-07-09 10:11:27 +0900

    render

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const { Client, GatewayIntentBits, SnowflakeUtil } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const BOT_TOKEN = process.env.BOT_TOKEN
+const http = require('http');
+const svr = http.createServer(handler);
+svr.listen(8081);
+function handler (req, res) {
+  console.log('url:', req.url)
+  console.log('method:', req.method)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end('<h1>Hello, World!</h1>\n')
+};
+
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const client = new Client(
     { intents: [
